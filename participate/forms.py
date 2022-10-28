73ec4bdbc0caaa865d2f0a8f7504c26f5f8dfc18
azmirahmadisha <azmi.rahmadisha@ui.ac.id>
2022-10-28 19:47:27 +0700

    stres

---
+++ participate/forms.py
@@ -0,0 +1,19 @@
+from unittest.util import _MAX_LENGTH
+from django import forms
+
+NGO_CHOICES = (
+('one', 'Delivering flyers'),
+('two', 'Delivering flyers'),
+('three', 'Delivering flyers'),)
+ 
+# creating a form
+class ParticipantsForm(forms.Form):
+    nama_pendaftar = forms.CharField(max_length=50, required=True, label="Full Name")
+    email_pendaftar = forms.CharField(max_length=50, required=True, label="E-mail")
+    phone_number = forms.CharField(max_length=12, required=True, label="Phone Number")
+    what_can_you_help_with = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
+                      choices=NGO_CHOICES, required=True, label="What can you help with?")
+
+    reason_to_participate = forms.CharField(widget=forms.Textarea(attrs={
+        'rows' : 4,
+        }), label="Why do you want to join us?")
--- participate/models.py
+++ participate/models.py
@@ -1,10 +1,16 @@
 
+from email.policy import default
+from operator import mod
+from statistics import mode
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 
+
 class Participants(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    nama_pendaftar = models.CharField(max_length=50)
-    email_pendaftar = models.CharField(max_length=50)
-    phone_number = models.IntegerField()
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    nama_pendaftar = models.CharField(max_length=50, default='')
+    email_pendaftar = models.CharField(max_length=50, default='')
+    phone_number = models.CharField(max_length=15, default='0')
+    what_can_you_help_with = models.CharField(max_length=50, verbose_name='What can you help with?', default="")
+    reason_to_participate = models.CharField(max_length=500, default='')
