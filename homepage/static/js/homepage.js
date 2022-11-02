3fb4c234af63e13d438380ccc7f81c6a05ee060f
Roy Maruli Tua Nababan <roy.maruli@ui.ac.id>
2022-11-02 15:14:46 +0700

    adding get info from session

--- homepage/static/js/homepage.js
+++ homepage/static/js/homepage.js
@@ -10,35 +10,51 @@ $(document).ready(() => {
             form.val();
             console.log(response);
             $('textarea[name=question]').val('');
+           
           },
-      }) 
+      })
+      add_last_question(); 
     })
   })
 
-  const getButton =  document.getElementById('get-count-btn');
-  
-  function gege(){
-    $.get(
-      '/get-campaign-sum/',
-      (res) => {
-        console.log(res);
-        $('#campaign-count').empty();
-        $('#count-detail').empty();
-        $('#campaign-count').append(
-          `
-          ${res}
-          `
-        )
-        $('#count-detail').append(
-          `
-          campaigns has already been started in Ecoist
+function add_last_question(){
+  $.get(
+    '/get_last_question/',
+    (res) => {
+      console.log("bisa kok");
+      $('#last-submit-question').empty();
+      $('#last-submit-question').append(
+        `
+        ${res}
+        `
+      );
+    }
+  )
+}
+const getButton =  document.getElementById('get-count-btn');
 
-          `
-        )
-      }
-      
-    )
-  }
+function gege(){
+  $.get(
+    '/get-campaign-sum/',
+    (res) => {
+      console.log(res);
+      $('#campaign-count').empty();
+      $('#count-detail').empty();
+      $('#campaign-count').append(
+        `
+        ${res}
+        `
+      )
+      $('#count-detail').append(
+        `
+        campaigns has already been started in Ecoist
 
-  $(`#get-count-btn`).attr('onclick', `gege()`);
+        `
+      )
+    }
+    
+  )
+}
+
+$(`#get-count-btn`).attr('onclick', `gege()`);
  
--- homepage/views.py
+++ homepage/views.py
@@ -16,14 +16,12 @@ def show_homepage(request):
     form = QuestionForm()
     context = {
             "form":form,
+            "last_question": request.session.get('last_login', 'have not submit a question yet')
         }
-    if request.user.is_authenticated and (not request.user.is_staff) :
-        context['last_login'].append(request.COOKIES.get('last_login'))
     return render(request,'homepage.html',context)
 
 @login_required(login_url='/login/')
 def submitquestion(request):
-    print("bekerja kapten")
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
@@ -34,11 +32,23 @@ def submitquestion(request):
             )
             new_question.save()
             response = serializers.serialize('json',[new_question])
+            request.session['last_question'] = str(datetime.datetime.now())
             return JsonResponse(response, safe=False)
     return HttpResponseBadRequest
 
-@csrf_exempt
+
 def getcampaignsum(request):
     if request.method == 'GET':
         data = str(Campaign.objects.count())
         return HttpResponse(data , content_type="text/plain")
+
+
+def get_last_question(request):
+    if request.method == 'GET':
+        data = str(request.session.get('last_question', "haven't submit a question yet"))
+        return HttpResponse(data , content_type="text/plain")
+
+@login_required(login_url='/login/')
+def show_json(request):
+    data = Question.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
