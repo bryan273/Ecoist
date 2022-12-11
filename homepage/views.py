d85cc3c5d6048e925fdee506bcb26989094dc908
Roy Maruli Tua Nababan <roy.maruli@ui.ac.id>
2022-12-12 03:11:20 +0700

    main page post and get finalization

--- homepage/views.py
+++ homepage/views.py
@@ -60,8 +60,10 @@ def getcampaignsum(request):
 @csrf_exempt
 def flutter_getcampaignsum(request):
     if request.method == 'GET':
-        data = (Campaign.objects.count())
-        return HttpResponse(data , content_type="text/plain")
+        print('---------------- campaign sum function called')
+        data = []
+        data.append({'count': Campaign.objects.count()}) 
+        return  JsonResponse(data, safe=False)
 
 
 def get_last_question(request):
@@ -80,4 +82,19 @@ def flutter_get_last_question(request):
 def show_json(request):
     data = Question.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+@csrf_exempt
+def flutter_getrecentquestions(request):
+    print('----------------function called')
+    count = Question.objects.count()
+    data = []
+    for x in range(3):
+        if (count-x)>0:
+            data.append({
+                'username' : str(Question.objects.get(pk=(count-x)).user),
+                'question': Question.objects.get(pk=(count-x)).question
+            })
+    return  JsonResponse(data, safe=False)
+
+
 
