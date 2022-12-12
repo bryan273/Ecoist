bdec96b4871391a9d1a6493ccf42f944713d4aa7
Roy Maruli Tua Nababan <roy.maruli@ui.ac.id>
2022-12-12 15:25:51 +0700

    test

--- homepage/urls.py
+++ homepage/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('flutter-submit-question/', flutter_submitquestion, name='flutter_submitquestion'),
     path('flutter-get-campaign-sum/', flutter_getcampaignsum, name='flutter_getcampaignsum'),
     path('flutter-get_last_question/', flutter_get_last_question, name="flutter_get_last_question"),
-    path('flutter-getrecentquestions', flutter_getrecentquestions, name ="flutter_getrecentquestions" ),
+    path('flutter-getrecentquestions/', flutter_getrecentquestions, name ="flutter_getrecentquestions" ),
     path('json/', show_json, name='show_json'),
 ]
